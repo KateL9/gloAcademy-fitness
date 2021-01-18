b6ecc20bff9331ab6230fa7b5da6dcfdfb4fbcdf
Kate Kunnikova <levchenkokaite@gmail.com>
2021-01-18 21:07:33 +0200

    fix: now select club dropdown is closed by clickng only outside dropdown layer

--- src/modules/selectClub.js
+++ src/modules/selectClub.js
@@ -2,17 +2,13 @@ const selectClub = () => {
     const clubDropdown = document.querySelector('.clubs-list>ul');
 
     let expanded = false;
+    clubDropdown.style.display = 'none';
 
     document.addEventListener('click', (event) => {
-        if (event.target.closest('.club-select')) {
-            if (!expanded) {
-                expanded = true;
-                clubDropdown.style.display = 'block';
-            } else {
-                expanded = false;
-                clubDropdown.style.display = 'none';
-            }
-        } else {
+        if (event.target.closest('.club-select') && clubDropdown.style.display == 'none' || event.target.matches('.clubs-list ul>li')) {
+            expanded = true;
+            clubDropdown.style.display = 'block';
+        } else if (expanded && !event.target.matches('.clubs-list ul')) {
             expanded = false;
             clubDropdown.style.display = 'none';
         }
